e59385dc87e610c7f04c4774d8c2471177ca4277
Atlas <kontakt@streetracingdenmark.com>
2021-02-05 09:47:58 +0100

    minor improvements
    -  Added semi colons, so prettiii.
    -  play/rhino function now returns an object with song/video info, as well as the dispatcher.
    -  Indenting fix for test.js

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
-const YOUTUBE = require('simple-youtube-api')
-const domain = require('./utils/domain')
-const log = require('./utils/log')
-const ytdl = require('ytdl-core')
+const YOUTUBE = require('simple-youtube-api');
+const log = require('./utils/log');
+const ytdl = require('ytdl-core');
 
 async function rhino(connection, keyword, key){
-    if (!key) return log("You Didn't Write Your Youtube API Key.")
-    let youtube = new YOUTUBE(key)
-    var videos = await youtube.searchVideos(keyword, 1)
+    if (!key) return log("You Didn't Write Your Youtube API Key.");
+    let youtube = new YOUTUBE(key);
+    var videos = await youtube.searchVideos(keyword, 1);
     const songInfo = await ytdl.getInfo(videos[0].url.replace(/<(.+)>/g, '$1'));
     const song = {
         id: songInfo.videoDetails.video_id,
-        url: songInfo.videoDetails.video_url
+        url: songInfo.videoDetails.video_url,
+        title: videos[0].title,
+        channel: videos[0].channel.title
     };
 
-    const dispatcher = connection.play(ytdl(song.url, { filter: 'audioonly', volume: 0.5}))
-    return dispatcher.setVolume(0.4)
+    const dispatcher = connection.play(ytdl(song.url, { filter: 'audioonly', volume: 0.5}));
+    dispatcher.setVolume(0.5);
+    return {
+    	...song,
+    	dispatcher
+    };
 }
 
 async function title(keyword, key){
-    if (!key) return log("You Didn't Write Your Youtube API Key.")
-    let youtube = new YOUTUBE(key)
-    var videos = await youtube.searchVideos(keyword, 1)
-    return videos[0].title
+    if (!key) return log("You Didn't Write Your Youtube API Key.");
+    let youtube = new YOUTUBE(key);
+    var videos = await youtube.searchVideos(keyword, 1);
+    return videos[0].title;
 }
 
 async function channel(keyword, key) {
-    if (!key) return log("You Didn't Write Your Youtube API Key.")
-    let youtube = new YOUTUBE(key)
-    var videos = await youtube.searchVideos(keyword, 1)
-    return videos[0].channel.title
+    if (!key) return log("You Didn't Write Your Youtube API Key.");
+    let youtube = new YOUTUBE(key);
+    var videos = await youtube.searchVideos(keyword, 1);
+    return videos[0].channel.title;
 }
 
 async function id(keyword, key) {
-    if (!key) return log("You Didn't Write Your Youtube API Key.")
-    let youtube = new YOUTUBE(key)
-    var videos = await youtube.searchVideos(keyword, 1)
-    return videos[0].id 
+    if (!key) return log("You Didn't Write Your Youtube API Key.");
+    let youtube = new YOUTUBE(key);
+    var videos = await youtube.searchVideos(keyword, 1);
+    return videos[0].id;
 }
 
-module.exports.play = rhino
-module.exports.id = id 
-module.exports.channel = channel 
-module.exports.title = title  
+module.exports.play = rhino;
+module.exports.id = id;
+module.exports.channel = channel;
+module.exports.title = title;
