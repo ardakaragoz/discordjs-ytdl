161990cd3007123488114e9cf31019b5515e6dbc
Atlas <kontakt@streetracingdenmark.com>
2021-02-05 10:05:50 +0100

    updated test to fit new improvements and stuff

--- test.js
+++ test.js
@@ -1,25 +1,25 @@
-const Discord = require('discord.js') // discord.js modülü tanımlıyoruz.
-const client = new Discord.Client() // client tanımalamsı
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const djsytdl = require("./");
 
-client.login('TOKEN')
+client.login("TOKEN");
 
-client.on('message', async message => {
+client.on("message", async message => {
     try {
         if (!message.guild) return;
 
-        if (message.content.startsWith('/play')) {
+        if (message.content.startsWith("!play")) {
             if (message.member.voice.channel) {
                 const connection = await message.member.voice.channel.join();
-                const args = message.content.split(' ').slice(1)
-                const play = require('./index')
-                play.play(connection, args.join(" "), "KEY")
-                const başlık = play.title(args.join(" "), "KEY")
-                başlık.then(title => message.channel.send(title))
+                const args = message.content.split(" ").slice(1);
+                const info = await djsytdl.play(connection, args.join(" "), "KEY"); // { title<String>: "some video title", channel<String>: "this is some channel", dispatcher<StreamDispatcher>: ... }
+                info.dispatcher.on("finish", () => connection.disconnect()); // Leave channel when the song is finished.
+                message.channel.send(`**Song Title:** *${info.title}*\n**Song Channel:** *${info.channel}*`);
             } else {
-                message.reply('You need to join a voice channel first!');
+                message.reply("You need to join a voice channel first!");
             }
         }
     } catch(e) {
-        console.log(e)
+        console.log(e);
     }
 });
